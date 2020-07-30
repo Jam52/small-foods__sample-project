7f12d779080876e4865b6c920aa1a659febeab0b
Jam52 <jamie.sajdak@gmail.com>
2020-07-30 13:01:37 -0400

    Feat: add section 1

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,9 @@
 import './styles/resets.scss';
 import './styles/base.scss';
 import './styles/nav.scss';
+import './styles/sections.scss';
 import './styles/header.scss';
+
 
 //import js
 import './js/header';
