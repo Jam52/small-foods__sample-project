2ddc56bc33facd5c6ca524a9c020646b67f5e2ed
Jam52 <jamie.sajdak@gmail.com>
2020-09-01 16:09:20 -0400

    Fix: use miniCssExtractPlugin.loader to ensure css is loaded in one style tag

--- src/client/index.js
+++ src/client/index.js
@@ -1,57 +1,46 @@
 //import styles
-import './styles/resets.scss';
-import './styles/base.scss';
-import './styles/nav.scss';
-import './styles/header.scss';
-import './styles/sections-main.scss';
-import './styles/section-download.scss';
-import './styles/section-divider-img.scss';
-import './styles/section-standards.scss';
-import './styles/section-landing.scss';
-import './styles/section-articles.scss';
-import './styles/section-community.scss';
-import './styles/section-footer.scss';
+import "./styles/import.scss";
 
-import './js/highlightNavElements';
-
+import "./js/highlightNavElements";
 
 //event listener for dropdown in nav
-const dropDownIcon = document.querySelector('.nav__dropdown-icon');
-const dropDownElements = document.querySelector('.nav__links');
-dropDownIcon.addEventListener('click', (event) => {
+const dropDownIcon = document.querySelector(".nav__dropdown-icon");
+const dropDownElements = document.querySelector(".nav__links");
+dropDownIcon.addEventListener("click", (event) => {
     event.preventDefault();
-    console.log('dropDown Clicked');
-    console.log(dropDownElements)
-    dropDownElements.classList.toggle('show');
-})
-
-
+    console.log("dropDown Clicked");
+    console.log(dropDownElements);
+    dropDownElements.classList.toggle("show");
+});
 
 //event listener to scrollTo sections
-const navElements = document.querySelectorAll('.nav__links');
-console.log(navElements)
-navElements.forEach(nav => {
-    nav.addEventListener('click', (event) => {
-        console.log('nav clicked')
+const navElements = document.querySelectorAll(".nav__links");
+console.log(navElements);
+navElements.forEach((nav) => {
+    nav.addEventListener("click", (event) => {
+        console.log("nav clicked");
         event.preventDefault();
-        const target = event.target.firstChild.textContent.toLowerCase().split(' ').join('-').replace('?','');
+        const target = event.target.firstChild.textContent
+            .toLowerCase()
+            .split(" ")
+            .join("-")
+            .replace("?", "");
         const section = document.querySelector(`#${target}`);
-        const offset = section.getBoundingClientRect().top + window.pageYOffset - 100;
+        const offset =
+            section.getBoundingClientRect().top + window.pageYOffset - 100;
         // section.scrollIntoView({behavior: "smooth"});
-        window.scrollTo({top: offset, behavior: 'smooth'});
-        dropDownElements.classList.remove('show');
-    })
-})
-
+        window.scrollTo({ top: offset, behavior: "smooth" });
+        dropDownElements.classList.remove("show");
+    });
+});
 
 //event listener for home icon
-const homeIcon = document.querySelector('.nav__home');
-homeIcon.addEventListener('click', (event) => {
+const homeIcon = document.querySelector(".nav__home");
+homeIcon.addEventListener("click", (event) => {
     event.preventDefault();
     window.scrollTo({
         top: 0,
         left: 0,
-        behavior: 'smooth'
-      });
-      
-})
+        behavior: "smooth",
+    });
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,47 +1,54 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-var ImageminPlugin = require('imagemin-webpack-plugin').default;
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/client/index.js',
+    mode: "production",
+    entry: "./src/client/index.js",
     devtool: false,
     module: {
         rules: [
             {
                 test: /\.html$/i,
-                loader: 'html-loader',
+                loader: "html-loader",
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                loader: "babel-loader",
             },
             {
                 test: /\.scss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
             },
             {
-                test: /\.(png|jp(e*)g|svg)$/,  
-                use: [{
-                    loader: 'url-loader',
-                    options: { 
-                        limit: 8000,
-                        name: 'images/[hash]-[name].[ext]'
-                    } 
-                }]
-            }
-        ]
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8000,
+                            name: "images/[hash]-[name].[ext]",
+                        },
+                    },
+                ],
+            },
+        ],
     },
+
     plugins: [
         new HtmlWebpackPlugin({
             minify: false,
-            filename: 'index.html',
-            template: './src/client/views/index.html'
+            filename: "index.html",
+            template: "./src/client/views/index.html",
         }),
         new CleanWebpackPlugin({
             // Simulate the removal of files
@@ -50,22 +57,13 @@ module.exports = {
             verbose: true,
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
+            protectWebpackAssets: false,
         }),
-        new MiniCssExtractPlugin ({filename:'[name].styles.css'}),
-        // new CopyWebpackPlugin([{
-        //     from: './src/client/images/bg 5.png',
-        //     to: path.resolve(__dirname, 'dist')
-        // }]),
-        // new ImageminPlugin({
-        //     disable: false,
-        //     pngquant: {
-        //       quality: [0.3, 0.5]
-        //     },
-        //   })
+        new MiniCssExtractPlugin({ filename: "[name].styles.css" }),
+        new OptimizeCSSAssetsPlugin({}),
     ],
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-      }
-}
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+};
