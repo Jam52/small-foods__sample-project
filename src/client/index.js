76d96922d13f3b1a59d824245db990ba132a650c
Jam52 <jamie.sajdak@gmail.com>
2020-09-07 18:10:49 -0400

    Fix: combine css, set header and divider images to rems

--- src/client/index.js
+++ src/client/index.js
@@ -1,46 +1,54 @@
 //import styles
-import "./styles/import.scss";
+import './styles/resets.scss';
+import './styles/_base.scss';
+import './styles/nav.scss';
+import './styles/header.scss';
+import './styles/sections-main.scss';
+import './styles/section-download.scss';
+import './styles/section-divider-img.scss';
+import './styles/section-standards.scss';
+import './styles/section-landing.scss';
+import './styles/section-articles.scss';
+import './styles/section-footer.scss';
 
-import "./js/highlightNavElements";
+import './js/highlightNavElements';
 
 //event listener for dropdown in nav
-const dropDownIcon = document.querySelector(".nav__dropdown-icon");
-const dropDownElements = document.querySelector(".nav__links");
-dropDownIcon.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("dropDown Clicked");
-    console.log(dropDownElements);
-    dropDownElements.classList.toggle("show");
+const dropDownIcon = document.querySelector('.nav__dropdown-icon');
+const dropDownElements = document.querySelector('.nav__links');
+dropDownIcon.addEventListener('click', (event) => {
+  event.preventDefault();
+  dropDownElements.classList.toggle('show');
 });
 
 //event listener to scrollTo sections
-const navElements = document.querySelectorAll(".nav__links");
+const navElements = document.querySelectorAll('.nav__links');
 console.log(navElements);
 navElements.forEach((nav) => {
-    nav.addEventListener("click", (event) => {
-        console.log("nav clicked");
-        event.preventDefault();
-        const target = event.target.firstChild.textContent
-            .toLowerCase()
-            .split(" ")
-            .join("-")
-            .replace("?", "");
-        const section = document.querySelector(`#${target}`);
-        const offset =
-            section.getBoundingClientRect().top + window.pageYOffset - 100;
-        // section.scrollIntoView({behavior: "smooth"});
-        window.scrollTo({ top: offset, behavior: "smooth" });
-        dropDownElements.classList.remove("show");
-    });
+  nav.addEventListener('click', (event) => {
+    console.log('nav clicked');
+    event.preventDefault();
+    const target = event.target.firstChild.textContent
+      .toLowerCase()
+      .split(' ')
+      .join('-')
+      .replace('?', '');
+    const section = document.querySelector(`#${target}`);
+    const offset =
+      section.getBoundingClientRect().top + window.pageYOffset - 100;
+    // section.scrollIntoView({behavior: "smooth"});
+    window.scrollTo({ top: offset, behavior: 'smooth' });
+    dropDownElements.classList.remove('show');
+  });
 });
 
 //event listener for home icon
-const homeIcon = document.querySelector(".nav__home");
-homeIcon.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-    });
+const homeIcon = document.querySelector('.nav__home');
+homeIcon.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
 });
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,70 +3,57 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-
 module.exports = {
-    mode: 'development',
-    entry: './src/client/index.js',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
+  mode: 'development',
+  entry: './src/client/index.js',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: 'images/[hash]-[name].[ext]',
             },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(png|jp(e*)g|svg)$/i,  
-                use: [{
-                    loader: 'url-loader',
-                    options: { 
-                        limit: 8000, // Convert images < 8kb to base64 strings
-                        name: 'images/[hash]-[name].[ext]'
-                    } 
-                }]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/client/views/index.html',
-            meta: {viewport: 'content="width=device-width, initial-scale=1'}
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        }),
+          },
+        ],
+      },
     ],
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/client/views/index.html'
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-      }
-}
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/client/views/index.html',
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
