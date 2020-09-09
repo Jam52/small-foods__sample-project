7cacbade34e579652b59d36484a6eb9f100f625d
Jam52 <jamie.sajdak@gmail.com>
2020-09-09 12:30:27 -0400

    Fix: use Autoprefixer to fix elements for IE 11

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,3 @@
-//import styles
 import './styles/resets.scss';
 import './styles/_base.scss';
 import './styles/nav.scss';
@@ -13,7 +12,6 @@ import './styles/section-footer.scss';
 
 import './js/highlightNavElements';
 
-//event listener for dropdown in nav
 const dropDownIcon = document.querySelector('.nav__dropdown-icon');
 const dropDownElements = document.querySelector('.nav__links');
 dropDownIcon.addEventListener('click', (event) => {
@@ -21,12 +19,10 @@ dropDownIcon.addEventListener('click', (event) => {
   dropDownElements.classList.toggle('show');
 });
 
-//event listener to scrollTo sections
 const navElements = document.querySelectorAll('.nav__links');
-console.log(navElements);
+
 navElements.forEach((nav) => {
   nav.addEventListener('click', (event) => {
-    console.log('nav clicked');
     event.preventDefault();
     const target = event.target.firstChild.textContent
       .toLowerCase()
@@ -36,13 +32,11 @@ navElements.forEach((nav) => {
     const section = document.querySelector(`#${target}`);
     const offset =
       section.getBoundingClientRect().top + window.pageYOffset - 100;
-    // section.scrollIntoView({behavior: "smooth"});
     window.scrollTo({ top: offset, behavior: 'smooth' });
     dropDownElements.classList.remove('show');
   });
 });
 
-//event listener for home icon
 const homeIcon = document.querySelector('.nav__home');
 homeIcon.addEventListener('click', (event) => {
   event.preventDefault();
